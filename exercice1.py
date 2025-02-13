2d3dcd84a8f50827aa6b9d1bbdaa3694832e39f5
mohab9248 <mohamad.abbas9248@gmail.com>
2025-02-13 17:05:51 +0200

    exercice 1 done

---
+++ exercice1.py
@@ -0,0 +1,13 @@
+age = int(input("please Enter Your age: "))
+
+
+if age >= 60:
+  print("The ticket price is 15$")
+elif age >= 40:
+  print("The ticket price is 25$")
+elif age >= 20:
+  print("The ticket price is 20$")
+elif age >= 10:
+  print("The ticket price is 10$")
+else:
+  print("The ticket price is 5$")
